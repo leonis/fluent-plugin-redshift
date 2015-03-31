0c70bb17916edf37a10b519af65dcd2368302063
hirakiuc <hirakiuc@gmail.com>
2015-03-31 23:09:07 +0900

    enable coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,15 @@
 if ENV['COVERAGE']
   require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
   SimpleCov.start do
     add_filter 'test/'
     add_filter 'pkg/'
     add_filter 'vendor/'
+    add_filter '.bundle'
   end
 end
